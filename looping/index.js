d9fce398296f780d1b8e306d65265ec462528488
jokoadi <jas.beastdjdj@gmail.com>
2022-10-20 23:26:55 +0700

    Fundamental

---
+++ looping/index.js
@@ -0,0 +1,69 @@
+// var stars = ''
+// var rows = 5
+
+// for (let i = 1; i <= rows; i++) {
+    
+//     for (let j = 1; j <= i; j++) {
+
+//         stars += " * "
+//     }
+
+//     stars += '\n'
+// }
+
+
+// console.log(stars);
+
+// // back 
+
+// var stars = ''
+// var rows = 5
+
+// for (let i = rows; i >= 1; i--) {
+    
+//     for (let j = 1; j <= i; j++) {
+
+//         stars += " * "
+//     }
+
+//     stars += '\n'
+// }
+
+
+// console.log(stars);
+
+
+//////////////
+//piramid////
+/////////////
+
+
+// var stars = ''
+
+// var rows = 5 
+
+// // loop i untuk membuat baris
+// // loop sebanyak nilai yg i simpan rows
+
+// for (let i = 1; i <= rows; i++) {
+    
+//     // loop j untuk menambah jml spasi tiap baris
+//     for (let j = (rows - i); j >= 1; j--) {
+
+//         stars += '   '
+//     }
+
+//     // loop k untuk menambah jml bintang setiap baris
+//     for (let k = 1; k <= (i - 1) * 2 + 1; k++) {
+
+//         stars += ' * '
+//     }
+
+//     stars += '\n'
+    
+// }
+
+// console.log(stars);
+
+
+
